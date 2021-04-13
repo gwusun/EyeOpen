7e67e5e184dccd831b7a3146314ebe394ebd47e7
sunwu <1228746736@qq.com>
2021-04-13 09:43:08 +0800

    feat: font size ++ and --

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,28 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <a-row :style="'text-align: center;font-size: '+fontSize+'px'">
+      <a-col :span="24">
+        <div style="height: 200px;font-size: 12px;">
+          使用方式：点击改变更换文字；盯着中间的字，同时注意两边的文字
+        </div>
+      </a-col>
+      <a-col :span="4">
+        <span v-show="isShow">  {{ word1 }}</span>
+      </a-col>
+      <a-col :span="16">
+        <span style="color: red;">看我</span>
+      </a-col>
+      <a-col :span="4" v-show="isShow">
+        <span v-show="isShow">  {{ word2 }}</span>
+      </a-col>
+      <a-col :span="24">
+        <div style="height: 200px;font-size: 12px;"></div>
+        <a-button @click="changeWord">改变</a-button>
+        <a-button @click="isShow=true">显示</a-button>
+        <a-button @click="fontSize++">加大文字</a-button>
+        <a-button @click="fontSize--">减少文字</a-button>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
@@ -35,24 +31,29 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      isShow: true,
+      word1: '我',
+      word2: '你',
+      fontSize: 12
+    }
+  }, methods: {
+    getRandomChineseWord() {
+      var _rsl = "";
+      var _randomUniCode = Math.floor(Math.random() * (40870 - 19968) + 19968).toString(16);
+      eval("_rsl=" + '"\\u' + _randomUniCode + '"');
+      return _rsl;
+    },
+    changeWord() {
+      this.isShow = true;
+      this.word1 = this.getRandomChineseWord();
+      this.word2 = this.getRandomChineseWord();
+      setTimeout(() => {
+        this.isShow = false
+      }, 500)
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
